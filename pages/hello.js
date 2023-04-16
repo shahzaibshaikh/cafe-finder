f8af9a4b7eb1544cfd3eead1493a2562d2f790f5
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-16 06:13:58 +0500

    👌 IMPROVE: removed hello component

--- pages/hello.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from '../styles/Hello.module.css';
-
-function Hello() {
-  return <div className={styles.container}>Hello World!</div>;
-}
-
-export default Hello;
