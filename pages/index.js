a058e7b4a5eef2354619a46446a3efd6e1822fef
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-16 16:50:50 +0500

    👌 IMPROVE: cleaned up more boilerplate

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
+import Banner from '@/components/Banner';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>Cafe Finder</h1>
+        <Banner />
       </main>
     </>
   );
