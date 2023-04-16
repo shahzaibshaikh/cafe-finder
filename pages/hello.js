9b538bc29d2111335e8144e19da2d9c08401c94e
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-16 06:07:41 +0500

    📦 NEW: added hello css module

---
+++ pages/hello.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/Hello.module.css';
+
+function Hello() {
+  return <div className={styles.container}>Hello World!</div>;
+}
+
+export default Hello;
