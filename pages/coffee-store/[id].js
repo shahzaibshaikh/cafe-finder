dfb9b7d58ef4b01971e8cc58f8b8310df524f5de
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-17 22:28:20 +0500

    👌 IMPROVE: added static link button

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,10 +1,16 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
 
 function CoffeeStore() {
   const router = useRouter();
 
-  return <div>Coffee store page: {router.query.id}</div>;
+  return (
+    <div>
+      Coffee store page: {router.query.id}
+      <Link href='/'>Back to home</Link>
+    </div>
+  );
 }
 
 export default CoffeeStore;
