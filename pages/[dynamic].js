849a2e7a9da756af9c7b143619e8de3d251c8d11
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-17 22:39:38 +0500

    👌 IMPROVE: dynamic page title

--- pages/[dynamic].js
+++ pages/[dynamic].js
@@ -1,9 +1,17 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React from 'react';
 
 function DynamicRoute() {
   const router = useRouter();
-  return <h1>Hi I'm a {router.query.dynamic} route.</h1>;
+  return (
+    <div>
+      <Head>
+        <title>{router.query.dynamic}</title>
+      </Head>
+      <h1>Hi I'm a {router.query.dynamic} route.</h1>
+    </div>
+  );
 }
 
 export default DynamicRoute;
