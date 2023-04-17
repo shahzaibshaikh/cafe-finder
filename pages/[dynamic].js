78dbe96db8d7ded207082db8692c59aad1fcc2d6
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-17 22:37:39 +0500

    👌 IMPROVE: added dynamic route with rendering

--- pages/[dynamic].js
+++ pages/[dynamic].js
@@ -1,7 +1,9 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
 function DynamicRoute() {
-  return <h1>Hi I'm a dynamic route.</h1>;
+  const router = useRouter();
+  return <h1>Hi I'm a {router.query.dynamic} route.</h1>;
 }
 
 export default DynamicRoute;
