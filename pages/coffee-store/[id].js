e4a330e09aa695589fe38855375f86ba32217280
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-17 22:32:01 +0500

    👌 IMPROVE: added a dynamic route

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -9,6 +9,7 @@ function CoffeeStore() {
     <div>
       Coffee store page: {router.query.id}
       <Link href='/'>Back to home</Link>
+      <Link href='/coffee-store/dynamic'>Back to dynamic</Link>
     </div>
   );
 }
