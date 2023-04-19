f95c0f93198bef1927c046bf89852084a213d624
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-19 22:37:38 +0500

    👌 IMPROVE: prerendering on dynamic page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,6 +1,16 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
+import coffeeStores from '../../data/coffee-stores.json';
+
+export async function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  return {
+    props: {
+      coffeeStore: coffeeStores.find(store => store.id === params.id)
+    }
+  };
+}
 
 function CoffeeStore() {
   const router = useRouter();
