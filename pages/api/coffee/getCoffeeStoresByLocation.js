901b83ebdfaac75c5e85133bf281add4d7ba83b2
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-26 22:00:04 +0500

    👌 IMPROVE: API call on serverless function

--- pages/api/coffee/getCoffeeStoresByLocation.js
+++ pages/api/coffee/getCoffeeStoresByLocation.js
@@ -2,6 +2,7 @@ import fetchCoffeeStores from '@/libs/fetchCoffeeStores';
 
 async function getCoffeeStoresByLocation(req, res) {
   const response = await fetchCoffeeStores();
+  if (response) res.status(200).json(response);
 }
 
 export default getCoffeeStoresByLocation;
